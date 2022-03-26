1b6129066f25d54574b7b1361269861dffb3336f
evertonsavio <everluca@outlook.com>
2022-03-26 12:22:39 -0300

    :sparkles: cast shadows

--- 02-realistic-render/src/script.js
+++ 02-realistic-render/src/script.js
@@ -28,6 +28,9 @@ const updateAllMaterials = () => {
             console.log(child)
             child.material.envMap = environmentMap
             child.material.envMapIntensity = debugObject.envMapIntensity
+            child.material.needsUpdate = true
+            child.castShadow = true
+            child.receiveShadow = true
         }
 
     })
@@ -67,7 +70,13 @@ gltfLoader.load(
 // Lights
 const directionalLight = new THREE.DirectionalLight("#ffffff", 3)
 directionalLight.position.set(0.25, 3, -2.25)
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024, 1024)
 scene.add(directionalLight)
+
+/* const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionalLightCameraHelper) */
 
 gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
 gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('lightX')
@@ -121,6 +130,8 @@ renderer.physicallyCorrectLights = true
 renderer.outputEncoding = THREE.sRGBEncoding
 renderer.toneMapping = THREE.ReinhardToneMapping
 renderer.toneMappingExposure = 3
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 /**
  * Animate
