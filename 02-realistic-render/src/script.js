189f410a41c72aa8a4536b7c4c0aa08d55212f77
evertonsavio <everluca@outlook.com>
2022-03-26 12:03:53 -0300

    :sparkles: tone mapping

--- 02-realistic-render/src/script.js
+++ 02-realistic-render/src/script.js
@@ -112,12 +112,15 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.physicallyCorrectLights = true
 renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 3
 
 /**
  * Animate
