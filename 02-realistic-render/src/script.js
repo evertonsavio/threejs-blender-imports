d8897697452feee85c055ec89dd794565434ad1d
evertonsavio <everluca@outlook.com>
2022-03-26 11:08:18 -0300

    :sparkles: model loader

--- 02-realistic-render/src/script.js
+++ 02-realistic-render/src/script.js
@@ -2,6 +2,11 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+
+//Loaders
+const gltfLoader = new GLTFLoader()
 
 /**
  * Base
@@ -14,6 +19,19 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+// Models
+gltfLoader.load(
+    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    (gltf) => {
+        gltf.scene.scale.set(10, 10, 10)
+        gltf.scene.position.set(0, -4, 0)
+        gltf.scene.rotation.y = Math.PI * 0.5
+        scene.add(gltf.scene)
+
+        gui.add(gltf.scene.rotation, 'y').min(- Math.PI).max(Math.PI).step(0.001).name('rotation')
+    }
+)
 
 // Lights
 const directionalLight = new THREE.DirectionalLight("#ffffff", 3)
